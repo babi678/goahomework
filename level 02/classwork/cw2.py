68418c53ba290beb8ad8239026f12f343d205629
babi678 <babi.mikeladze2009@gmail.com>
2025-07-07 14:28:56 +0400

    commit

---
+++ level 02/classwork/cw2.py
@@ -0,0 +1,15 @@
+num1=20
+num2=40
+print(num1+num2)
+
+
+num1=20
+num2=30
+num3=num1+num2
+print(num3,num1+num2)
+num4=num1-num2
+print(num4)
+num5=num1*num2
+print(num5)
+num6=num1/num2
+print(num6)
