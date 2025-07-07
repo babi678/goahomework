ae0484b8c99e6dc003b266d9f3f42e43c70d0fe1
babi678 <babi.mikeladze2009@gmail.com>
2025-07-07 15:48:54 +0400

    commit

---
+++ level 02/homework/hw2.py
@@ -0,0 +1,62 @@
+# davaleba 1
+num1=20
+num2=50
+print(num1+num2)
+print(num1-num2)
+print(num1/num2)
+print(num1*num2)
+
+# davaleba 2
+phone="samsung"
+car="BMW"
+country="Georgia"
+animal="dog"
+
+height=180
+age=20
+length=50
+weight=40
+
+price=4.5
+temperature=36.5
+speed=129.7
+IQ=167.05
+
+print(phone)
+print(car)
+print(country)
+print(animal)
+print(height)
+print(age)
+print(length)
+print(weight)
+print(price)
+print(temperature)
+print(speed)
+print(IQ)
+
+# davaleba 3
+name1="babi"
+name2="gvanca"
+print(name1+name2)
+# სტრინგ ფუნქციების შეკრებას ეწოდება კონკატინაცია
+
+# davaleba 4
+name1="nini"
+name2="nika"
+name3="luka"
+name4="saba"
+name5="manana"
+print(name1+name2+name3+name4+name5)
+
+# davaleba 5
+num3=37
+num4=29.5
+num5=num3*num4
+num6=num3+num4
+num7=num3-num4
+num8=num3/num4
+
+age1=40
+age1=30
+print(age1)
