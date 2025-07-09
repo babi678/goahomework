57a9be09edbc8e8bce41adf53ca6155ae946fb84
babi678 <babi.mikeladze2009@gmail.com>
2025-07-09 19:34:33 +0400

    commit

---
+++ level 03/homework/hw3.py
@@ -0,0 +1,47 @@
+# davaleba 1
+num_1=20
+num_2=30
+num_3=num_1+num_2
+num_4=num_1-num_2
+num_5=num_1*num_2
+num_6=num_1/num_2
+print(num_3,num_4,num_5,num_6)
+
+# davaleba 2
+name="babi"
+name="saeki"
+name="nakata"
+name="kafka"
+name="joni"
+print(name)
+
+# davaleba 3
+car="toyota"
+Car="opel"
+cAr="BMW"
+caR="porche"
+CAR="ferrari"
+print(car,Car,cAr,caR,CAR)
+
+# davaleba 4
+name_2="giorgi"
+# რიცხვით ვერ დაიწყებოდა ცვლადი
+user_name="bubunauri"
+# { ეს შეცდომაა
+user_name="goga"
+# ბრჭყალები აკლდა
+user_surname="axalaia"
+# არასწორი ტირეთი იყო გამოყოფილი და ბრჭყალები აკლდა
+
+# davaleba 5
+nickname="tevza"
+feeling="uyvars"
+job="tevzaoba"
+x="yvelaferze"
+y="metad"
+print(nickname+feeling+job+x+y)
+
+# davaleba 6
+NAME="babi"
+number=10
+print(NAME*number)
