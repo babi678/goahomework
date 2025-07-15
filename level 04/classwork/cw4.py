ff1c08eb5d91381f927361b23f144ad8e5889dff
babi678 <babi.mikeladze2009@gmail.com>
2025-07-15 10:23:30 +0400

    commit

---
+++ level 04/classwork/cw4.py
@@ -0,0 +1,16 @@
+# name=input("enter your name :")
+# print(name)\
+
+
+# number1=4.5
+# number2="babi"
+# number3=True
+# number4=7
+# print(type(4.5))
+# print(type("babi"))
+# print(type(True))
+# print(type(7))
+
+number1=30
+number2=40
+print(int(30)+int(40))
---
+++ level 04/homework/hw4.py
@@ -0,0 +1,58 @@
+# დავალება 2
+# input - არის ფუნქცია რომელიც საშუალებას გვაძლევს მომხმარებელს შევატანინოთ ტერმინალში რაიმე და ჩვენ შეგვეძლება მისი დამუშავება 
+# output - არის მომხმარებლისთვის პასუხი 
+
+# დავალება 3
+name=input("babi")
+print(type("babi"))
+print(name)
+
+# დავალება 4
+# integer
+num1=2
+num2=3
+num3=4
+num4=5
+num5=6
+# string
+name1="nika"
+name2="luka"
+name3="gio"
+name4="mari"
+name5="nia"
+# float
+num6=4.5
+num7=4.7
+num8=3.6
+num9=5.8
+num10=5.6
+
+# დავალება 5
+car="BMW"
+age=15
+height=1.80
+print(type("BMW"))
+print(type(15))
+print(type(1.80))
+
+# დავალება 6
+x=input("enter your name:")
+y=input("enter your friend's name:")
+print(x+y)
+
+# დავალება 7
+z=int(input("enter your age:"))
+c=int(input("enter your height:"))
+v=int(input("enter your birth year:"))
+b=int(input("enter your fav number:"))
+n=int(input("enter your weight:"))
+total=z+c+v+b+n
+print(int(total)/5)
+
+# დავალება 8
+f=input("enter your name:")
+g=input("enter your surname:")
+h=input("enter your age:")
+j=input("enter your height:")
+k=input("enter your weight:")
+print(f+g+h+j+k)
