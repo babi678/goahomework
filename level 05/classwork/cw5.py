f62266d8d654f4c86534a63fc2b0140a075feb55
babi678 <babi.mikeladze2009@gmail.com>
2025-07-16 13:43:15 +0400

    commit

---
+++ level 05/classwork/cw5.py
@@ -0,0 +1,5 @@
+num1=input("enter first number:")
+num2=input("enter second number:")
+print(int(num1)>int(num2))
+print(int(num1)<int(num2))
+print(int(num1)==int(num2))
---
+++ level 05/homework/hw5.py
@@ -0,0 +1,55 @@
+# დავალება 1
+# ჩვენ ვისწავლეთ შედარების ოპერატორები > , < , == 
+
+# დავალება 2
+num1=int(input("enter first random number:"))
+num2=int(input("enter second random number:"))
+print(num1>num2)
+print(num1<num2)
+print(num1==num2)
+
+# დავალება 3
+animal=input("enter your favorite animal:")
+car=input("enter your favorite car:")
+fruit=input("enter your favorite fruit:")
+coda=input("enter your favorite coda:")
+book=input("enter your favorite book:")
+print(animal+car+fruit+coda+book)
+
+# დავალება 4
+num3=int(input("enter your age:"))
+num4=int(input("enter your height:"))
+num5=int(input("enter your favorite number:"))
+num6=int(input("enter your weight:"))
+total=num3+num4+num5+num6
+print(int(total)/4)
+
+# დავალება 5
+x="ball"
+y=50
+z=6.7
+t=True
+print(type(x))
+print(type(y))
+print(type(z))
+print(type(t))
+
+# დავალება 6
+name1="nika"
+name2="nini"
+print(name1==name2)
+
+# დავალება 7
+NUM1="30"
+NUM2="50"
+NUM3="70"
+NUM4="80"
+sum=int(NUM1)+int(NUM2)+int(NUM3)+int(NUM4)
+print(sum)
+
+# დავალება 8
+num7=45
+num8=29
+num9=48
+sum1=str(num7)+str(num8)+str(num9)
+print(sum1)
