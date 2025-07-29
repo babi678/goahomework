66bde2380523cda82a4dca9aa73802b72352e64f
babi678 <babi.mikeladze2009@gmail.com>
2025-07-29 16:25:20 +0400

    commit

---
+++ level 08/classwork/cw8.py
@@ -0,0 +1,11 @@
+# print(50>30 and 40<23)
+# print(67==29 and 57<70)
+# print(40==40 and 56<20)
+
+# print(40>30 or 20<10)
+# print(30>40 or 40==40)
+# print(20==20 or 40>30)
+
+for i in range(20):
+    print("babi mikeladze")
+
---
+++ level 08/homework/hw8.py
@@ -0,0 +1,70 @@
+# დავალება 1
+print(5>4 and 5<9)
+print(30==30 and 30<50)
+print(23>20 and 7==7)
+print(40>20 or 5>7)
+print(39==59 or 46>20)
+
+print(5==30 or 49>90)
+print(40>89 or 5==6)
+print(56==40 and 47>9)
+print(20>12 and 50==60)
+print(90>60 and 40==80)
+
+# დავალება 2
+# sequancing - კოდის თანმიმდევრობა
+# iteration - გამეორება 
+# selection - არჩევანი / არჩევა
+
+# დავალება 3
+for i in range(5):
+    print(i)
+
+# დავალება 4
+# for loop - არის ფუნქცია რომელიც საშუალებას გვაძლევს ტერმინალში გამოვიტანოთ ნებისმიერი რამე დიდი რაოდენობით ბევრი წერის გარეშე
+
+# დავალება 5
+# range() - ამ ფუნქციას გადაეცემა რაოდენობა რამდენჯერაც გვინდა რომ ტერმინალში გამოვიდეს შედეგი
+# ასევე ქმნის რიცხვებს 0 დან იმ რიცხვამდე ციფრებს
+
+# დავალება 6
+for i in range(3):
+    print("ferrari")
+
+# დავალება 7
+for i in range(100):
+    print("mikeladze")
+
+# დავალება 8
+for i in range(46):
+    print("red")
+
+# დავალება 9
+for i in range(32):
+    print("b")
+
+# დავალება 10
+name_1=input("enter your name:")
+name_2=input("enter your fav color:")
+name_3=input("enter random stuff:")
+number_1=int(input("enter random number:"))
+print(name_1+name_2+name_3+str(number_1))
+
+# დავალება 11
+name="nini"
+number=20
+float=3.4
+boolean=True 
+print(type(name))
+print(type(number))
+print(type(float))
+print(type(boolean))
+
+# დავალება 12
+num_1=int(input("enter your age:"))
+num_2=int(input("enter your height:"))
+num_3=int(input("enter random number:"))
+num_4=int(input("enter your weight:"))
+print(num_1+num_2+num_3+num_4)
+
+
